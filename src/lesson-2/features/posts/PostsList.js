5947a4038ecea1aa9d2ff6929b5338455055f551
sumonaung23 <sumonaung575@gmail.com>
2023-10-31 00:00:22 +0630

    added PostsExcerpt components and modified content for asyhchronous action

--- src/lesson-2/features/posts/PostsList.js
+++ src/lesson-2/features/posts/PostsList.js
@@ -1,32 +1,37 @@
-import { useSelector } from 'react-redux'
-import { selectAllPosts } from './postSlice';
-import PostAuthor from './PostAuthor';
-import TimeAgo from './TimeAgo';
-import ReactionButtons from './ReactionButtons';
+import { useSelector, useDispatch } from 'react-redux'
+import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from './postSlice';
+import PostsExcerpt from './PostsExcerpt';
 
 import './post.css';
+import { useEffect } from 'react';
 
 const PostsList = () => {
+    const dispatch = useDispatch();
+
     const posts = useSelector(selectAllPosts);
+    const postStatus = useSelector(getPostsStatus);
+    const error = useSelector(getPostsError);
 
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date)) 
+    useEffect(() => {
+        if(postStatus === 'idle') {
+            dispatch(fetchPosts())
+        }
+    }, [postStatus, dispatch])
 
-    const renderedPosts = orderedPosts.map(post => (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0,100)}</p>
-            <p className='postCredit'>
-                <PostAuthor userId={post.userId} />
-                <TimeAgo timestamp={post.date} />
-            </p>
-            <ReactionButtons post={post} />
-        </article>
-    ))
+    let content;
+    if (postStatus === 'loading') {
+        content = <p>"Loading..."</p>;
+    } else if (postStatus === 'succeeded') {
+        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+        content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
+    } else if (postStatus === 'failed') {
+        content = <p>{error}</p>;
+    }
 
     return (
         <section className='postList'>
             <h2>Posts</h2>
-            {renderedPosts}
+            {content}
         </section>
     )
 }
