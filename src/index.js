c47e3f82604b7b0a7d6d173f886a2c911d3fb5f7
sumonaung23 <sumonaung575@gmail.com>
2023-10-31 14:31:37 +0630

    imported fetchUers and updated store by dispatch

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { store } from './lesson-2/app/store';
 import { Provider } from 'react-redux';
+import { fetchUsers } from './lesson-2/features/users/usersSlice';
 
 import App from './App';
+
+store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
