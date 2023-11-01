4e4270cc59c5492ec1ede03cf3525b93d9208421
sumonaung23 <sumonaung575@gmail.com>
2023-11-01 21:25:17 +0630

    modified to route and dispatch the store with fetchPosts

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { store } from './lesson-2/app/store';
 import { Provider } from 'react-redux';
+import { fetchPosts } from './lesson-2/features/posts/postSlice';
 import { fetchUsers } from './lesson-2/features/users/usersSlice';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import App from './App';
 
+store.dispatch(fetchPosts());
 store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path='/' element={<App />}/>
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
