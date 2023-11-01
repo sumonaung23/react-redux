22206a4a5ca5601de426b1a8a59b34de5a16f07a
sumonaung23 <sumonaung575@gmail.com>
2023-11-01 21:16:28 +0630

    modified by adding Routes for each pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import PostsList from './lesson-2/features/posts/PostsList';
 import AddPostForm from './lesson-2/features/posts/AddPostForm';
+import SinglePostPage from './lesson-2/features/posts/SinglePostPage';
+import EditPostForm from './lesson-2/features/posts/EditPostFrom';
+import Layout from './lesson-2/components/Layout';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
-    <main className="app">
-      <AddPostForm />
-      <PostsList />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path='edit/:postId' element={<EditPostForm />} />
+        </Route>
+
+      </Route>
+    </Routes>
   );
 }
 
