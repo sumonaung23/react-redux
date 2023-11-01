c4c0252cdb3f648a328797905d3c59a8838ffdea
sumonaung23 <sumonaung575@gmail.com>
2023-11-01 21:18:35 +0630

    added Link for routing single Post

--- src/lesson-2/features/posts/PostsExcerpt.js
+++ src/lesson-2/features/posts/PostsExcerpt.js
@@ -1,13 +1,16 @@
 import PostAuthor from "./PostAuthor"
 import TimeAgo from "./TimeAgo"
 import ReactionButtons from "./ReactionButtons"
+import { Link } from "react-router-dom"
 
 const PostsExcerpt = ({ post }) => {
     return (
         <article>
             <h3>{post.title}</h3>
-            <p>{post.body.substring(0, 100)}</p>
+            <p className="excerpt">{post.body.substring(0, 75)}...</p>
             <p className='postCredit'>
+                <Link to={`post/${post.id}`}>View Post</Link>
+
                 <PostAuthor userId={post.userId} />
                 <TimeAgo timestamp={post.date} />
             </p>
