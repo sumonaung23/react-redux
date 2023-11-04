e425f80c0336a0a0e72ec1804eaa5cdd25e00623
sumonaung23 <sumonaung575@gmail.com>
2023-11-04 21:17:34 +0630

    imported useDispatch, useSelector form redux and functions from postSlice, and by using imported elements and functionn, modified list Link and added button along with function using dispatch

--- src/lesson-2/components/Header.js
+++ src/lesson-2/components/Header.js
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
+import { increaseCount, getCount } from "../features/posts/postSlice";
 
 const Header = () => {
+    const dispatch = useDispatch()
+    const count = useSelector(getCount)
+
     return (
         <header>
             <h1>Redux Blog</h1>
@@ -8,7 +13,12 @@ const Header = () => {
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="post">Post</Link></li>
+                    <li><Link to="user">Users</Link></li>
                 </ul>
+                <button
+                    onClick={() => dispatch(increaseCount())}>
+                        {count}
+                </button>
             </nav>
         </header>
     )
