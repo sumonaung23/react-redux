5616300a0179942e0c3152aeb13d8026cf53ec97
sumonaung23 <sumonaung575@gmail.com>
2023-11-04 21:15:00 +0630

    imported UserList, Navigate and added new user route along with nested routes and added Navigate route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import AddPostForm from './lesson-2/features/posts/AddPostForm';
 import SinglePostPage from './lesson-2/features/posts/SinglePostPage';
 import EditPostForm from './lesson-2/features/posts/EditPostFrom';
 import Layout from './lesson-2/components/Layout';
-import { Routes, Route } from 'react-router-dom';
+import UsersList from './lesson-2/features/users/usersList';
+import UserPage from './lesson-2/features/users/UserPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css';
 
@@ -19,6 +21,13 @@ function App() {
           <Route path=":postId" element={<SinglePostPage />} />
           <Route path='edit/:postId' element={<EditPostForm />} />
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
 
       </Route>
     </Routes>
