8222331e16715909496a6618ef2693f719c37878
sumonaung23 <sumonaung575@gmail.com>
2023-11-04 21:21:53 +0630

    imported selectPostIds and modified variables with it, changed the orderedPosts variagle with content with map function

--- src/lesson-2/features/posts/PostsList.js
+++ src/lesson-2/features/posts/PostsList.js
@@ -1,11 +1,11 @@
 import { useSelector } from 'react-redux'
-import { selectAllPosts, getPostsStatus, getPostsError } from './postSlice';
+import { selectPostIds, getPostsStatus, getPostsError } from './postSlice';
 import PostsExcerpt from './PostsExcerpt';
 
 import './post.css';
 
 const PostsList = () => {
-    const posts = useSelector(selectAllPosts);
+    const orderPostIds = useSelector(selectPostIds);
     const postStatus = useSelector(getPostsStatus);
     const error = useSelector(getPostsError);
 
@@ -13,8 +13,7 @@ const PostsList = () => {
     if (postStatus === 'loading') {
         content = <p>"Loading..."</p>;
     } else if (postStatus === 'succeeded') {
-        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-        content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
+        content = orderPostIds.map(postId => <PostsExcerpt key={postId} postId={postId} />)
     } else if (postStatus === 'failed') {
         content = <p>{error}</p>;
     }
