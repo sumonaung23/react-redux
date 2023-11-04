1724cd16e0db2c7a7c67acbb1137902e0bfe1453
sumonaung23 <sumonaung575@gmail.com>
2023-11-04 21:19:46 +0630

    imported useSelector from redux, selectPostById from postSlice and defined a post variable with useSelector for postId

--- src/lesson-2/features/posts/PostsExcerpt.js
+++ src/lesson-2/features/posts/PostsExcerpt.js
@@ -3,7 +3,12 @@ import TimeAgo from "./TimeAgo"
 import ReactionButtons from "./ReactionButtons"
 import { Link } from "react-router-dom"
 
-const PostsExcerpt = ({ post }) => {
+import { useSelector } from "react-redux"
+import { selectPostById } from "./postSlice"
+
+const PostsExcerpt = ({ postId }) => {
+    const post = useSelector(state => selectPostById(state, postId))
+    
     return (
         <article>
             <h3>{post.title}</h3>
