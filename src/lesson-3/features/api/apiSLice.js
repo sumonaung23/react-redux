b29c8846a1ad38e38aa8b826467066818f8be1dd
sumonaung23 <sumonaung575@gmail.com>
2023-11-05 22:07:15 +0630

    created featuresfolder for todo and api files

---
+++ src/lesson-3/features/api/apiSLice.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const apiSlice = createApi ({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+    endpoints: (builder) => ({
+        getTodos: builder.query({
+            query: () => '/todos',
+        })
+    })
+})
+
+export const {
+    useGetTodosQuery
+} = apiSlice
