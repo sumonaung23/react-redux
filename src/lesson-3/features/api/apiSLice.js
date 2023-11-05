582eb98525968b52df2ffe53b649615244d04300
sumonaung23 <sumonaung575@gmail.com>
2023-11-06 00:40:32 +0630

    created apiSlice with createApi from RTK and make the endpoints

--- src/lesson-3/features/api/apiSLice.js
+++ src/lesson-3/features/api/apiSLice.js
@@ -6,10 +6,35 @@ export const apiSlice = createApi ({
     endpoints: (builder) => ({
         getTodos: builder.query({
             query: () => '/todos',
-        })
+        }),
+        addTodo: builder.mutation({
+            query: (todo) => ({
+                url: '/todos',
+                method: 'POST',
+                body: todo
+            })
+        }),
+        updateTodo: builder.mutation ({
+            query: (todo) => ({
+                url: `/todos/${todo.id}`,
+                method: "PATCH",
+                body: todo
+            })
+        }),
+        deleteTodo: builder.mutation({
+            query: ({ id }) => ({
+                url: `/todos/${id}`,
+                method: 'DELETE',
+                body: id
+            }),
+            invalidatesTags: ['Todos']
+        }),
     })
 })
 
 export const {
-    useGetTodosQuery
+    useGetTodosQuery,
+    useAddTodoMutation,
+    useUpdateTodoMutation,
+    useDeleteTodoMutation
 } = apiSlice
