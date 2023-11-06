91744b8397050b9943af0bc44a68aea72420d1fd
sumonaung23 <sumonaung575@gmail.com>
2023-11-07 00:00:13 +0630

    created a store

---
+++ src/lesson-4/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/api/apiSlice";
+import usersReducer from '../features/users/usersSlice';
+
+
+export const store = configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        users: usersReducer
+    },
+    middleware: getDefaultMiddleware => 
+        getDefaultMiddleware().concat(apiSlice.middleware)
+})
