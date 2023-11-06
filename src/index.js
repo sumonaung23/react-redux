3b4ed8e5565895b92117d07952c7f064cd4e3d15
sumonaung23 <sumonaung575@gmail.com>
2023-11-06 11:24:39 +0630

    commented out previous lessons and then imported ApiProvider, apiSlice and modified root

--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from './lesson-2/app/store';
-import { Provider } from 'react-redux';
-import { fetchPosts } from './lesson-2/features/posts/postSlice';
-import { fetchUsers } from './lesson-2/features/users/usersSlice';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { apiSlice } from './lesson-3/features/api/apiSlice';
 
 import App from './App';
 
-store.dispatch(fetchPosts());
-store.dispatch(fetchUsers());
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/*' element={<App />}/>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root'))
+  .render(
+    <React.StrictMode>
+      <ApiProvider api={apiSlice}>
+        <App />
+      </ApiProvider>
+    </React.StrictMode>
+  )
 
 
-/* ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/*' element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-); */
