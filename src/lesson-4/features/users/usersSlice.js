d4f265fdb085146f47323c6efc3ddf8c01044ad6
sumonaung23 <sumonaung575@gmail.com>
2023-11-07 23:15:31 +0630

    updated userSlice with createSelector and createEntityAdapter function from RTK, and apiSlice

--- src/lesson-4/features/users/usersSlice.js
+++ src/lesson-4/features/users/usersSlice.js
@@ -1,29 +1,45 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import {
+    createSelector,
+    createEntityAdapter
+} from "@reduxjs/toolkit";
+import { apiSlice } from "../api/apiSlice";
 
-const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+const usersAdapter = createEntityAdapter()
 
-const initialState = []
+const initialState = usersAdapter.getInitialState()
 
-export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
-    const response = await axios.get(USERS_URL);
-    return response.data
-})
-
-const usersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {},
-    extraReducers(builder) {
-        builder.addCase(fetchUsers.fulfilled, (state, action) => {
-            return action.payload;
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        getUsers: builder.query({
+            query: () => '/users',
+            transformResponse: responseData => {
+                return usersAdapter.setAll(initialState, responseData)
+            },
+            providesTags: (result, error, arg) => [
+                { type: 'User', id: "LIST" },
+                ...result.ids.map(id => ({ type: 'User', id }))
+            ]
         })
-    }
+    })
 })
 
-export const selectAllUsers = (state) => state.users;
+export const {
+    useGetUsersQuery
+} = usersApiSlice
 
-export const selectUserById = (state, userId) =>
-    state.users.find(user => user.id === userId)
+// returns the query result object
+export const selectUsersResult = usersApiSlice.endpoints.getUsers.select()
 
-export default usersSlice.reducer
+// Creates memoized selector
+const selectUsersData = createSelector(
+    selectUsersResult,
+    usersResult => usersResult.data // normalized state object with ids & entities
+)
+
+//getSelectors creates these selectors and we rename them with aliases using destructuring
+export const {
+    selectAll: selectAllUsers,
+    selectById: selectUserById,
+    selectIds: selectUserIds
+    // Pass in a selector that returns the posts slice of state
+} = usersAdapter.getSelectors(state => selectUsersData(state) ?? initialState)
